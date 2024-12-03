1654805567f40c81c359e84e5c44f25fc154fa5d
JIYEON HWANG <ghkdwldus0807@gmail.com>
2024-12-03 22:50:28 +0900

    cors error fix

--- fresh_scan/src/main/java/HCI/fresh_scan/controller/ImageProcessingController.java
+++ fresh_scan/src/main/java/HCI/fresh_scan/controller/ImageProcessingController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
 public class ImageProcessingController {
 
